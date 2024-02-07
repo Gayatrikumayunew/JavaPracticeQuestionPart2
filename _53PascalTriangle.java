112ba210207bd0c3d538e03b50e98dad6520d49d
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2024-02-08 00:02:18 +0530

    Add files via upload

---
+++ _53PascalTriangle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author gayatri
+ */
+public class _53PascalTriangle {
+    public static void main(String [] ar){
+        //1
+        //11
+        //121
+        //1331
+        //14641
+        int n=5;
+        System.out.println(1<<(n-1));
+       // System.out.println(Math.pow(2, n-1));
+    }
+}
