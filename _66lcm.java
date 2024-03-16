0cc6bed412e48f1197d9ee46b7e29eaa72fa1d1b
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2024-03-17 00:09:49 +0530

    Add files via upload

---
+++ _66lcm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author gayatri
+ */
+public class _66lcm {
+    public static void main(String[] args) {
+        int ans=lcm(2,7);
+        System.out.println(ans);
+    }
+    static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    static int lcm(int a,int b){
+        return a*b/gcd(a,b);
+    }
+}
