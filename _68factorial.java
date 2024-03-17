2ba588c85b4d97256594cb4f5f86e8658e862970
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2024-03-17 15:14:07 +0530

    Add files via upload

---
+++ _68factorial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author gayatri
+ */
+public class _68factorial {
+    public static void main(String[] args) {
+        int ans=factorial(5);
+        System.out.println(ans);
+    }
+    static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+}
