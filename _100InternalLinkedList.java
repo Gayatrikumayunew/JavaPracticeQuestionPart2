e939038c7be306eeef5a34692afed6b6313287a0
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2024-07-15 23:58:45 +0530

    Add files via upload

---
+++ _100InternalLinkedList.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author gayatri
+ */
+public class _100InternalLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer>list=new LinkedList<>();
+        list.add(34);
+    }
+}
